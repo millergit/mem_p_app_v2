0d2a1b9768e95585e50399700e5568f8b3b4790e
millergit <matthew@Carl.localdomain>
2025-07-26 14:38:25 -0400

    icon name change

---
+++ create_icon.js
@@ -0,0 +1,12 @@
+// Simple script to create a basic contacts icon
+const fs = require('fs');
+
+// Create a simple 1024x1024 PNG with basic contacts design
+// This is a temporary solution - we can improve it later
+const createBasicIcon = () => {
+  console.log('Creating basic contacts icon...');
+  console.log('For now, the existing icon.png will be used.');
+  console.log('The app name "Contacts" is now updated in app.json');
+};
+
+createBasicIcon();
